4c6a8ef0bdb80eec049cbb441bfec82b6ffcedda
titomon98 <arturomonterroso3@gmail.com>
2025-09-05 17:07:55 -0600

    Primer commit con proyecto vacio

---
+++ src/test/java/arquitectura/capas/CapasApplicationTests.java
@@ -0,0 +1,13 @@
+package arquitectura.capas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
